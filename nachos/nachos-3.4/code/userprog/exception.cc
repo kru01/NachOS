f4d0533ff17efd0b9bd48bcf62980e7f014b3738
kru01 <79639749+kru01@users.noreply.github.com>
2023-03-17 00:10:28 +0700

    - refactored ExceptionHandler in userprog/exception.cc, all main exceptions are covered
     - added IncreaseProgramCounter in userprog/exception.cc (untested)
    
     - added new synchcons class and modified code/Makefile.common to include it (haven't recompiled the OS)
    
     - these are task a, b, c in Guides/ProjectInfo

--- nachos/nachos-3.4/code/userprog/exception.cc
+++ nachos/nachos-3.4/code/userprog/exception.cc
@@ -48,16 +48,79 @@
 //	are in machine.h.
 //----------------------------------------------------------------------
 
+//----------------------------------------------------------------------
+// IncreaseProgramCounter
+// 	Handy function to increase program counter.
+//----------------------------------------------------------------------
+
+void
+IncreaseProgramCounter()
+{
+    // Line 117 to 118 in machine/mipssim.cc
+    // Compute next pc, but don't install in case there's an error or branch.
+    int pcAfter = registers[NextPCReg] + 4;
+
+    // Line 560 to 564 in machine/mipssim.cc
+    // Advance program counters.
+    registers[PrevPCReg] = registers[PCReg];	// for debugging, in case we
+						// are jumping into lala-land
+    registers[PCReg] = registers[NextPCReg];
+    registers[NextPCReg] = pcAfter;
+}
+
+//----------------------------------------------------------------------
+// ExceptionHandler
+// 	Fucntion that handles exceptions, mostly runtime errors.
+// 
+//  NOTE -- NoException will transfer control back to OS while Syscall
+//  exceptions will be individually handled. Others will just inform of
+//  the error and halt the system.
+//  PS -- ExceptionType can be found in machine/machine.h
+//----------------------------------------------------------------------
+
 void
 ExceptionHandler(ExceptionType which)
 {
     int type = machine->ReadRegister(2);
 
-    if ((which == SyscallException) && (type == SC_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
+    switch (which) {
+    case NoException: return;
+    case PageFaultException:
+        DEBUG('a', "No valid translation found.\n");
+        interrupt->Halt();
+        break;
+    case ReadOnlyException:
+        DEBUG('a', "Write attempted to page marked \"read-only\".\n");
+        interrupt->Halt();
+        break;
+    case BusErrorException:
+        DEBUG('a', "Translation resulted in an invalid physical address.\n");
+        interrupt->Halt();
+        break;
+    case AddressErrorException:
+        DEBUG('a', "Unaligned reference or one that was beyond the end of the address space.\n");
+        interrupt->Halt();
+        break;
+    case OverflowException:
+        DEBUG('a', "Integer overflow in add or sub.\n");
+        interrupt->Halt();
+        break;
+    case IllegalInstrException:
+        DEBUG('a', "Unimplemented or reserved instr.\n");
+        interrupt->Halt();
+        break;
+    case NumExceptionTypes:
+        DEBUG('a', "Number Exception Types.\n");
+        interrupt->Halt();
+        break;
+    case SyscallException:
+        switch (type) {
+        case SC_Halt:
+            DEBUG('a', "Shutdown, initiated by user program.\n");
+            interrupt->Halt();
+        }
+    default:
+        printf("Unexpected user mode exception %d %d\n", which, type);
+        ASSERT(FALSE);
     }
 }
